0ff1e442eac1cc38525a6117cb14854a6c529ced
Droogans <droogans@gmail.com>
2013-07-31 10:17:20 -0500

    Implement Flycheck recommendations.

--- init.el
+++ init.el
@@ -1,7 +1,12 @@
+;;; package -- Summary
+
+;;; Commentary:
+
+;;; Code:
 (make-directory "~/.emacs.d/autosaves/" t)(when (>= emacs-major-version 24)
   (require 'package)
   (package-initialize)
-  (add-to-list 'package-archives 
+  (add-to-list 'package-archives
 	       '("melpa" . "http://melpa.milkbox.net/packages/") t))
 
 (make-directory "~/.emacs.d/autosaves/" t)
@@ -46,7 +51,7 @@
 
 ;Custom commands
 (defun reload-init ()
-"reload init.el without restarting"
+"Reload init.el without restarting."
     (interactive)
       (load-file "~/.emacs.d/init.el"))
 
@@ -69,3 +74,21 @@
 (setq ring-bell-function 'ignore)
 (setq global-linum-mode t)
 (add-hook 'after-init-hook #'global-flycheck-mode)
+(defvar flycheck-check-syntax-automatically)
+(setq flycheck-check-syntax-automatically '(save))
+
+;extra mode configs
+(eval-after-load "color-theme"
+  '(progn
+     (color-theme-initialize)
+     (color-theme-dark-laptop)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(put 'downcase-region 'disabled nil)
+(provide 'init)
+;;; init.el ends here
