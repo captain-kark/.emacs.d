54bab3cc54b96cce9db551d90ce06d7e5a31f020
Droogans <droogans@gmail.com>
2013-10-28 12:51:36 -0500

    Resize Emacs smaller than default screen size.

--- init.el
+++ init.el
@@ -109,8 +109,8 @@
 
 ;extra mode configs
 (setq-default indent-tabs-mode nil)
-(add-to-list 'default-frame-alist '(height . 80))
-(add-to-list 'default-frame-alist '(width . 143))
+(add-to-list 'default-frame-alist '(height . 60))
+(add-to-list 'default-frame-alist '(width . 120))
 (put 'downcase-region 'disabled nil)
 (put 'upcase-region 'disabled nil)
 
