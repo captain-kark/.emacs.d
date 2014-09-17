de462e92a50a523440fce33925e56f8342006c0d
Droogans <droogans@gmail.com>
2014-09-17 11:19:22 -0500

    Elpa update.

---
+++ elpa/rings-20140102.1536/rings-autoloads.el
@@ -0,0 +1,18 @@
+;;; rings-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("rings-pkg.el" "rings.el") (21486 19780
+;;;;;;  531493 0))
+
+;;;***
+
+(provide 'rings-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; rings-autoloads.el ends here
