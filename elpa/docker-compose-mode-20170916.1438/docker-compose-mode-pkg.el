047db2b173ea90d1e7e199fff914297106c803f0
Droogans <droogans@gmail.com>
2017-10-03 13:38:13 -0400

    Add docker modes

---
+++ elpa/docker-compose-mode-20170916.1438/docker-compose-mode-pkg.el
@@ -0,0 +1,1 @@
+(define-package "docker-compose-mode" "20170916.1438" "Major mode for editing docker-compose files" '((emacs "24.3") (dash "2.12.0") (yaml-mode "0.0.12")) :commit "ae25b0e01394f94995f4b0ceaab1e04eb4b9acbd" :url "https://github.com/meqif/docker-compose-mode" :keywords '("convenience"))
