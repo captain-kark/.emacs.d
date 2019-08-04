43bc178a66e08528c24230afc8ccdb1aeb36d602
captain-kark <andrew.yurisich@nbx.com>
2019-08-04 22:19:24 +0200

    kubernetes mode

--- init.el
+++ init.el
@@ -77,7 +77,7 @@
  '(helm-ff-newfile-prompt-p t)
  '(package-selected-packages
    (quote
-    (plantuml-mode flycheck-golangci-lint flycheck-gometalinter flymake-go flycheck-pycheckers pyvenv backup-each-save exec-path-from-shell company-jedi use-package helm-projectile pipenv projectile pyenv-mode tide rainbow-mode nvm multiple-cursors markdown-mode less-css-mode helm-ag go-mode git-gutter-fringe dockerfile-mode docker-compose-mode company-terraform base16-theme adaptive-wrap)))
+    (kubernetes sudo-edit plantuml-mode flycheck-golangci-lint flycheck-gometalinter flymake-go flycheck-pycheckers pyvenv backup-each-save exec-path-from-shell company-jedi use-package helm-projectile pipenv projectile pyenv-mode tide rainbow-mode nvm multiple-cursors markdown-mode less-css-mode helm-ag go-mode git-gutter-fringe dockerfile-mode docker-compose-mode company-terraform base16-theme adaptive-wrap)))
  '(plantuml-jar-path "~/.emacs.d/elpa/plantuml-mode-20190510.657/plantuml.jar")
  '(savehist-mode t))
 
@@ -91,6 +91,10 @@
 (set-face-background 'git-gutter:deleted  "#803C3C")
 
 (add-hook 'json-mode-hook (lambda () (interactive) (setq tab-width 2)))
+
+(use-package kubernetes
+  :ensure t
+  :commands (kubernetes-overview))
 
 (autoload 'markdown-mode "markdown-mode"
   "Major mode for editing Markdown files" t)
